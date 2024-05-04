60e68f342624720fb62183a7e16c91d43c654671
Sandro Santamaria Ramos <R000001@inmobiliariahuertas.local>
2024-05-04 10:18:37 -0500

    Agregando animacion  al cubo

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import * as THREE from 'three';
 
 //1 - Crear una Escena
 const escena = new THREE.Scene();
+escena.background = new THREE.Color(0x2a3b4c) //agregando color de fonodo
 
 //2 Agregar la camara
 const camara = new THREE.PerspectiveCamera(
@@ -19,5 +20,30 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight) //Tamaño del navegador.
 document.body.appendChild(renderer.domElement)//Agregando render al documento HTML
 
+// 5-1 Agregando geometria
+const geometria = new THREE.BoxGeometry();
+// 5-2 Material
+const material =  new THREE.MeshBasicMaterial({color: 0x00f000, wireframe:true});
+// 5 Mesh esta compuesto por una geometria y un material
+const cubo = new THREE.Mesh(geometria,material)
+
+//6 - Agregando el cubo a la escena
+escena.add(cubo)
+
+//Cambiando la posicion de nuestra camara zoom misntras menos número, la figura es mas grande
+camara.position.z =5
+
 // 4 Agregar la Escena y la camara al render.
+//renderer.render(escena,camara)
+
+//7 crear anumacion
+const animacion = ()=>{
+    //Bucle para que se repita la animacion
+    requestAnimationFrame(animacion)
+    cubo.rotation.x +=0.01;
+    cubo.rotation.z +=0.01;
+    //Actualizaremos el render en bucle
 renderer.render(escena,camara)
+
+}
+animacion()
