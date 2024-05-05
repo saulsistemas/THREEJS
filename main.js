86d5c543054a06aa2d9ce413080ac1fd106e65f5
Sandro Santamaria Ramos <R000001@inmobiliariahuertas.local>
2024-05-05 15:03:37 -0500

    configurando controles de movimiento

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
 
 //Comando para correr proyecto npx vite
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 
 //1 - Crear una Escena
 const escena = new THREE.Scene();
-escena.background = new THREE.Color("skyblue") //agregando color de fonodo
-escena.fog = new THREE.Fog(0x76456c, 0.1, 8) //efecto de sombra
+//escena.background = new THREE.Color("skyblue") //agregando color de fonodo
+//escena.fog = new THREE.Fog(0x76456c, 0.1, 8) //efecto de sombra
+
 
 //cargaremos imagen
-    //const carga = new THREE.TextureLoader()
-    //carga.load('./img/custom-sky.png',(textura)=>{
-    //    escena.background = textura
-    //})
+//const carga = new THREE.TextureLoader()
+//carga.load('./img/custom-sky.png', (textura) => {
+//    escena.background = textura
+//})
 
 //2 Agregar la camara
 const camara = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth/window.innerHeight,
+    window.innerWidth / window.innerHeight,
     0.1, //Defecto si no se manda
     2000  //Defecto si no se manda
 )
 
 
-const newCamara = new THREE.OrthographicCamera(
-    5,-5,5,-5,3,10
-    //window.innerWidth/window.innerHeight,
-    //3,
-    //10
-)
-const helper = new THREE.CameraHelper(newCamara)
-escena.add(helper)
+
 //3 Agregar al render
 
 const renderer = new THREE.WebGLRenderer();
@@ -40,31 +35,39 @@ document.body.appendChild(renderer.domElement)//Agregando render al documento HT
 // 5-1 Agregando geometria
 const geometria = new THREE.BoxGeometry();
 // 5-2 Material
-const material =  new THREE.MeshBasicMaterial({color: 0x00f000, wireframe:true});
+const material = new THREE.MeshBasicMaterial({ color: 0x00f000, wireframe: true });
 // 5 Mesh esta compuesto por una geometria y un material
-const cubo = new THREE.Mesh(geometria,material)
-cubo.position.z = -5
+const cubo = new THREE.Mesh(geometria, material)
 //6 - Agregando el cubo a la escena
 escena.add(cubo)
 
 //Cambiando la posicion de nuestra camara zoom misntras menos número, la figura es mas grande
-//camara.position.z = 5 //unidades saliendo del computador
+camara.position.z = 5 //unidades saliendo del computador
 
+//activamos el orbicontrols para girar, zoom la camara, funciona automaticamentes
+const controles = new OrbitControls(camara,renderer.domElement)
+
+//propiedades opcionales
+controles.minDistance = 3
+controles.maxDistance =10
+//controles.enableZoom=false
+//controles.enableRotate=false
+controles.enableDamping=true
+controles.dampingFactor = 0.5
+//controles.maxPolarAngle = Math.PI /10
+controles.screenSpacePanning = true;
 // 4 Agregar la Escena y la camara al render.
 //renderer.render(escena,camara)
-let i = 0
+
 //7 crear anumacion
-const animacion = ()=>{
+const animacion = () => {
     //Bucle para que se repita la animacion
     requestAnimationFrame(animacion)
-    camara.lookAt(newCamara.position)
-    camara.position.x = Math.cos(i)*30
-    camara.position.z = Math.sin(i)*30
-    i +=0.01
-    //cubo.rotation.x +=0.01;
-    //cubo.rotation.z +=0.01;
+
+    cubo.rotation.x += 0.01;
+    cubo.rotation.z += 0.01;
     //Actualizaremos el render en bucle
-renderer.render(escena,camara)
+    renderer.render(escena, camara)
 
 }
 animacion()
